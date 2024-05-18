f0b4929a0211968ae472e6c971beba955ac4b983
JD-MBP <jashansingh29@gmail.com>
2024-05-18 19:37:44 -0400

    ;

--- serverJSP/settings.py
+++ serverJSP/settings.py
@@ -53,8 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'utilities.middlware.CrossDomainSessionMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
+
 
 
 ]
