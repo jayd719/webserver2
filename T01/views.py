22cd4400d1240b1efbcc2987478c6b16b5f814d6
JD-MBP <jashansingh29@gmail.com>
2024-05-22 14:09:46 -0400

    ;

--- T01/views.py
+++ T01/views.py
@@ -25,5 +25,32 @@ def createCookies(requests):
 
 
 
+def projects(requests):
+    DATA={
+    'companyName':COMPANYNAME,
+    'pythonLibs': PythonLibs.objects.all(),
+    'machines':CNC.objects.all()
+}   
+    return render(requests,f'projects/projects.html',DATA)
+def aboutme(requests):
+    return render(requests,'aboutpage/main.html')
+@csrf_exempt
+def locationFromCords(request):
+    """request - the HTTP request object (HttpRequest)
+    Returns:
+        JSON response indicating the success or failure of the operation (JsonResponse)
+    -------------------------------------------------------
+    """
+    if request.method == 'POST':
+        try:
+            cordinates = json.loads(request.body.decode('utf-8'))['data']
+            return JsonResponse({'success': True})
+        except json.JSONDecodeError as e:
+            return JsonResponse({'error': 'Invalid JSON format'}, status=400)
+    return JsonResponse({'error': 'Invalid request method'}, status=405)
+def engineeringPage(requests):
+    return render(requests,f'EngineeringPage/engineeringPage.html')
+def gantchart(requests):
+    return render(requests,'ganttChart/ganttchart.html')
 
 
