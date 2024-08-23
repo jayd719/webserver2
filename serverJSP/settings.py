9c7a08666b73e00b228e05e4f591ef5f6436db56
JD-MBP <jashansingh29@gmail.com>
2024-08-23 14:55:26 -0400

    ;

--- serverJSP/settings.py
+++ serverJSP/settings.py
@@ -132,8 +132,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'https://jayd719.github.io/staticfiles/'
-# STATIC_URL ='/static/'
+# STATIC_URL = 'https://jayd719.github.io/staticfiles/'
+STATIC_URL ='/static/'
 STATIC_ROOT = BASE_DIR/'staticfiles'
 
 STATICFILES_DIRS = [
