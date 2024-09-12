ef8d4da4ad3ba2459b53be826b4019c85be9f49a
JD-MBP <jashansingh29@gmail.com>
2024-09-12 13:06:07 -0400

    k

--- T04/urls.py
+++ T04/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import resume1,resume2
 urlpatterns = [
-    path('1/', resume1,name="resume-one"),
-    path('2/', resume2,name="resume-one"),
+    path('', resume1,name="resume-one"),
+    path('resume/', resume2,name="resume-two"),
 ]
