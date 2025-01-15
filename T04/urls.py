8cf6a199034bcf11fdb4108b58a5092c250a1871
jd <jashansingh29@gmail.com>
2025-01-15 11:34:58 -0500

    k

--- T04/urls.py
+++ T04/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from django.shortcuts import redirect
 from .views import *
+
 urlpatterns = [
-    path('resume/1/', resume1, name="resume"),
-    path('resume/2/', resume2, name="resume-two"),
-    path('aboutme/', about_me, name='about-me'),
-    path('image-processing-toolkit/', open_cv_project_1, name='imageprocessing'),
-    path('computer-graphics-3d-renderer/', cp411_project, name='renderer'),
+    path("resume/1/", resume1, name="resume"),
+    path("resume/2/", resume2, name="resume-two"),
+    path("aboutme/", about_me, name="about-me"),
+    path("image-processing-toolkit/", open_cv_project_1, name="imageprocessing"),
+    path("computer-graphics-3d-renderer/", cp411_project, name="renderer"),
+    path("pres", pres, name="temp"),
 ]
--- T04/views.py
+++ T04/views.py
@@ -1,17 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def resume1(request):
-    return render(request, 'resume1.html')
+    return render(request, "resume1.html")
 
 
 def resume2(request):
-    return render(request, 'resume2.html')
+    return render(request, "resume2.html")
 
 
 def about_me(request):
-    return render(request, 'aboutMe/index.html')
-    
+    return render(request, "aboutMe/index.html")
+
 
 def open_cv_project_1(request):
     return render(request, "ProjectReports/openCV1.html")
@@ -19,3 +19,9 @@ def open_cv_project_1(request):
 
 def cp411_project(request):
     return render(request, "ProjectReports/CP411.html")
+
+
+def pres(request):
+    return redirect(
+        "https://docs.google.com/presentation/d/1aESC8HSqfXATkgoLYQXO24lREU6zhjDBWmhg3BD7Iqw/edit?usp=sharing"
+    )
